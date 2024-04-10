c062bead22ad62142b4f8d8f39369b42f34267a8
cheesedanish1 <pearlmans33737@thejec.org>
2024-04-10 19:11:19 -0400

    pushed localhost api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // eslint-disable-next-line
-// const { PRODUCTION_APP_URI /*API_URI*/ } = require("../constants.json");
-const { API_URI } = require("../constants.json");
+const { PRODUCTION_APP_URI /*API_URI*/ } = require("../constants.json");
+// const { API_URI } = require("../constants.json");
 
-// const API_URI = PRODUCTION_APP_URI;
+const API_URI = PRODUCTION_APP_URI;
 
 export async function getUser() {
   return axios({
